ad6d1565eabd2221cf46a3d66863acbc902d1886
GraydeeSi <wiseguy0002@gmail.com>
2017-08-11 10:47:43 -0400

    Tortoise Potion

---
+++ Buffs/TortoiseBuff.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace Wildlife.Buffs
+{
+    public class TortoiseBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+             DisplayName.SetDefault("Tortoise protection");
+            Description.SetDefault("Increases defense with lower health");
+       
+            Main.pvpBuff[Type] = true;
+            Main.buffNoTimeDisplay[Type] = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+           float defBoost = (float)(player.statLifeMax2 - player.statLife) / (float)player.statLifeMax2 * 22f;
+			player.statDefense += (int)defBoost;
+
+        }
+    }
+}
---
+++ Items/Potions/TortoisePotion.cs
@@ -0,0 +1,52 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System;
+using System.Collections.Generic;
+
+
+namespace Wildlife.Items.Potions
+{
+	public class TortoisePotion : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Tortoise Potion");
+           Tooltip.SetDefault("Gives extra defense if you're low on health.");
+
+        }
+		 public override void SetDefaults()
+        {
+         
+            item.UseSound = SoundID.Item3;
+            item.useStyle = 2;
+            item.useTurn = true;
+            item.useAnimation = 17;
+            item.useTime = 17;
+            item.maxStack = 30;
+            item.consumable = true;
+            item.width = 12;
+            item.height = 30;
+          
+            item.value = 2500;
+            item.rare = 5;
+            item.buffType = mod.BuffType("TortoiseBuff");
+            item.buffTime = 15000;
+           // return;
+        }
+		
+       
+		public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.BottledWater, 1);
+            recipe.AddIngredient(null, "Turtle", 1);
+			recipe.AddIngredient(null, "CatTail", 1);
+            recipe.AddTile(TileID.Bottles);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+        
+	}
+}
