854b4ea2997e05ada364952978abe1a97fedc371
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-08-12 08:41:09 +0100

    Blue Fairy Buff

---
+++ Buffs/BlueFairyBuff.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace Wildlife.Buffs
+{
+    public class BlueFairyBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+             DisplayName.SetDefault("Fairy's Knowledge");
+            Description.SetDefault("10% Reduced Mana Cost");
+       
+            Main.pvpBuff[Type] = true;
+            Main.buffNoTimeDisplay[Type] = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.manaCost -= 0.10f;
+			
+        }
+    }
+}
