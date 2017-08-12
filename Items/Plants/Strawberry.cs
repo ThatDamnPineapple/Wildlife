83fbebda23927e2ebca7beb23e74f27fe17a0bfe
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-08-12 03:34:45 +0100

    Update Strawberry.cs

--- Items/Plants/Strawberry.cs
+++ Items/Plants/Strawberry.cs
@@ -4,8 +4,6 @@ using Terraria.ModLoader;
 using Terraria.ID;
 using System;
 using System.Collections.Generic;
-
-
 namespace Wildlife.Items.Plants
 {
 	public class Strawberry : ModItem
@@ -13,42 +11,25 @@ namespace Wildlife.Items.Plants
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Strawberry");
-            //Tooltip.SetDefault("Restores 30 life");
-
+           Tooltip.SetDefault("'Simply Delicious'");
         }
-		public override void SetDefaults()
-		{
-			item.UseSound = SoundID.Item3;
+		 public override void SetDefaults()
+        {
+         
+            item.UseSound = SoundID.Item2;
             item.useStyle = 2;
             item.useTurn = true;
-            item.useAnimation = 20;
-            item.useTime = 20;
+            item.useAnimation = 17;
+            item.useTime = 17;
             item.maxStack = 30;
-           item.potion = true;
             item.consumable = true;
-			item.healLife = 30;
-            item.value = 3500;
-			
-            item.rare = 1;
-            return;
-		}
-		/*public override bool CanUseItem(Player player)
-		{
-			if (player.FindBuffIndex(BuffID.PotionSickness)>=0)
-			{
-				return false;
-			}
-			return true;
-			
-		}*/
-        public override bool UseItem(Player player)
-        {
+            item.width = 12;
+            item.height = 30;
           
-			player.AddBuff(BuffID.PotionSickness, 3600);
-			
-            
-            return true;
-        }
-        
+            item.value = 2500;
+            item.rare = 0;
+            item.buffType = item.BuffType("26");
+            item.buffTime = 5000;
+           // return;
 	}
 }
