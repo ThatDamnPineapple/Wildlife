b00ccf122141867e48ee2aac0c83c6429a6d5632
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-08-12 13:24:03 +0100

    ascension buff

---
+++ Buffs/Ascension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace Wildlife.Buffs
+{
+    public class Ascension : ModBuff
+    {
+        public override void SetDefaults()
+        {
+             DisplayName.SetDefault("Ascension");
+            Description.SetDefault("Increased Wing Time");
+       
+            Main.pvpBuff[Type] = true;
+            Main.buffNoTimeDisplay[Type] = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.wingTime += 40f;
+			
+        }
+    }
+}
