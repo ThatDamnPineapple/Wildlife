fdbfff4bc5006a24240b0d7d8ecc0a5389a433f0
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-08-13 09:51:32 +0100

    removed swinging

--- Items/Plants/Bamboo.cs
+++ Items/Plants/Bamboo.cs
@@ -24,8 +24,8 @@ namespace Wildlife.Items.Plants
 			item.useTurn = true;
 			item.autoReuse = true;
 			item.useAnimation = 15;
-			item.useTime = 10;
-			item.useStyle = 1;
+			//item.useTime = 10;
+			//item.useStyle = 1;
 			item.value = 500;
 			//item.createTile = mod.TileType("Bamboo");
 			//item.consumable = true;
