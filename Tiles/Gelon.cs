2e035a7d802ec3e3459728c77ac3ba18ee65e156
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-09-07 17:30:26 +0100

    Update Gelon.cs

--- Tiles/Gelon.cs
+++ Tiles/Gelon.cs
@@ -16,7 +16,7 @@ namespace Wildlife.Tiles
             Main.tileMergeDirt[Type] = true;
             Main.tileBlockLight[Type] = true;
             Main.tileLighted[Type] = true;
-            AddMapEntry(new Color(200, 0, 0));
+            AddMapEntry(new Color(32, 55, 187));
 
             TileObjectData.newTile.CoordinateHeights = new int[]
             {
