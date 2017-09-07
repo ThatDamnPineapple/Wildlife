35ae89b8b38cfd23b3eb6f8f5f5f9e81a5ee581b
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-09-07 17:29:19 +0100

    Update Jaderose.cs

--- Tiles/Jaderose.cs
+++ Tiles/Jaderose.cs
@@ -16,7 +16,7 @@ namespace Wildlife.Tiles
             Main.tileMergeDirt[Type] = true;
             Main.tileBlockLight[Type] = true;
             Main.tileLighted[Type] = false;
-            AddMapEntry(new Color(200, 0, 0));
+            AddMapEntry(new Color(138, 172, 115));
 
             TileObjectData.newTile.CoordinateHeights = new int[]
             {
