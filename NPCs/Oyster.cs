739c35704489956dae1899b9c74ace1f9fccf0ec
Paradiscal <Paradiscal@outlook.com>
2018-05-30 00:18:30 +0100

    a

--- NPCs/Oyster.cs
+++ NPCs/Oyster.cs
@@ -79,7 +79,13 @@ namespace Wildlife.NPCs
 				open = false;
 			}
 		}
-		
+		public override void NPCLoot()
+        {
+            if (Main.rand.Next(2) == 0)
+            {
+                Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("OysterPearl"));
+            }
+        }
 		public override void HitEffect(int hitDirection, double damage)
         {
             if (npc.life <= 0)
