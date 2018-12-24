91fa01f8d0fb23d2cd0b2962a568df70fa8710d8
five0five <44749261+five0five@users.noreply.github.com>
2018-12-24 18:09:47 -0500

    phoenix

---
+++ Items/Animals/Phoenix.cs
@@ -0,0 +1,67 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+using System;
+
+namespace Wildlife.Items.Animals
+{
+	public class Phoenix : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Phoenix");
+            
+
+        }
+		public override void SetDefaults()
+		{
+			item.width = 26;
+			item.height = 22;
+			item.maxStack = 99;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.noMelee = true;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.noUseGraphic = true;
+			item.consumable = true;
+			item.value = 10000;
+		}
+		  public override bool UseItem(Player player)
+        {
+            NPC.NewNPC((int)player.Center.X, (int)player.Center.Y, mod.NPCType("Phoenix"));
+            return true;
+        }
+		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float  scale, int whoAmI) 	
+		{
+			Texture2D texture;
+			texture = Main.itemTexture[item.type];
+			spriteBatch.Draw
+			(
+				mod.GetTexture("Items/Animals/Phoenix"),
+				new Vector2
+				(
+					item.position.X - Main.screenPosition.X + item.width * 0.5f,
+					item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
+				),
+				new Rectangle(0, 0, texture.Width, texture.Height),
+				Color.White,
+				rotation,
+				texture.Size() * 0.5f,
+				scale, 
+				SpriteEffects.None, 
+				0f
+			);
+		}	       
+	}
+}
---
+++ NPCs/Phoenix.cs
@@ -0,0 +1,79 @@
+using Terraria;
+using System;
+using Terraria.ID;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace Wildlife.NPCs
+{
+    public class Phoenix : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Scorching Phoenix");
+            Main.npcFrameCount[npc.type] = 5;
+        }
+        public override void SetDefaults()
+        {
+            npc.width = 32;
+            npc.height = 24;
+            npc.damage = 0;
+            npc.defense = 0;
+            npc.lifeMax = 5;
+			npc.chaseable = false;
+			Main.npcCatchable[npc.type] = true;
+            npc.catchItem = (short)mod.ItemType("Phoenix");
+            npc.HitSound = SoundID.NPCHit5;
+            npc.DeathSound = SoundID.NPCDeath3;
+            npc.value = 0f;
+			npc.noGravity = true;
+			animationType = NPCID.Bird;
+			aiType = NPCID.Bird;  
+			npc.aiStyle = 24;
+            npc.knockBackResist = .25f;
+			//banner = npc.type;
+			//bannerItem = mod.ItemType("SnakeBanner");
+  
+        }
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return spawnInfo.player.ZoneUnderworldHeight ? 0.05f : 0f;
+        }
+		public override void HitEffect(int hitDirection, double damage)
+        {
+            if (npc.life <= 0)
+            {
+                if (Main.rand.Next(4) == 0)
+                {
+			    NPC.NewNPC((int)(npc.position.X), (int)(npc.position.Y), (mod.NPCType("Phoenix")));
+			    NPC.NewNPC((int)(npc.position.X), (int)(npc.position.Y), (mod.NPCType("Phoenix")));
+				Main.PlaySound(SoundID.Item4, npc.position);
+			    Dust.NewDust(npc.position, npc.width, npc.height, 158);
+			    Dust.NewDust(npc.position, npc.width, npc.height, 158);
+			    Dust.NewDust(npc.position, npc.width, npc.height, 158);
+			    Dust.NewDust(npc.position, npc.width, npc.height, 158);
+                }
+			Dust.NewDust(npc.position, npc.width, npc.height, 158);
+            }
+        }
+        public override void AI()
+		{
+			Vector3 RGB = new Vector3(0.25f, 0.15f, 0.10f);
+			npc.TargetClosest();
+			float multiplier = 0.75f;
+			float max = 2.25f;
+			float min = 1.0f;
+			RGB *= multiplier;
+			if(RGB.X > max) {
+				multiplier = 0.5f;
+			}
+			if(RGB.X < min) {
+				multiplier = 1.5f;
+			}
+			Lighting.AddLight(npc.position, RGB.X, RGB.Y, RGB.Z);
+			
+		} 
+    }
+}
